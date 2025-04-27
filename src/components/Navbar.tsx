b5cfb51ddbfbb2f48e15945502161f506c8d312d
Iyanuoluwapo Adesanya <96719103+Aiyanu@users.noreply.github.com>
2025-04-27 18:29:07 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -55,7 +55,7 @@ const Navbar = () => {
         </div>
         <div className="flex items-center space-x-4">
           <Button variant={"outline"}>
-            <a href={"/assets/RESUME(1).PDF"} download>
+            <a href={"/assets/RESUME.pdf"} download>
               Download CV
             </a>
           </Button>
